fb2ef946cfef33c759e155678c58ddb10f6ae500
Trikon-3ways <ayush.vermaOfficial2003@gmail.com>
2025-09-29 22:22:14 +0530

    triung agian

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001/api';
+
+const API_BASE_URL =
+  import.meta.env.VITE_API_URL || 'https://suchbliss-personal-deployment-backend.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
